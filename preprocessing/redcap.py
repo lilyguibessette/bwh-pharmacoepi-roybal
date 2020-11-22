77e5083852daba5181b24b43820649c16e263018
lilyguibessette <lilyguibessette@gmail.com>
2020-11-21 21:24:16 -0500

    recap

--- preprocessing/redcap.py
+++ preprocessing/redcap.py
@@ -25,40 +25,40 @@ def update_patient_dict_redcap(unique_study_ids_list_redcap, redcap, pt_dict):
             # i.e. CATEGORY from redcap = 1 => age 18-25 (rough example) ->
             # shouldn't be continuous, needs to be string to become categorical to personalizer
             new_patient = Patient(str(record_id),
-                              row['start_date'],
+                              row['start_date'].values[0],
                               datetime.now(),
                               0,
                               False,
                               int(row['age']),
-                              row['sex'],
-                              row['num_years_dm_rx'],
-                              row['hba1c'],
-                              row['race___1'],
-                              row['race___2'],
-                              row['race___3'],
-                              row['race___4'],
-                              row['race___5'],
-                              row['race___6'],
-                              row['race___7'],
-                              row['num_physicians'],
-                              row['num_rx'],
-                              row['concomitant_insulin_use'],
-                              row['automaticity'],
-                              row['pt_activation'],
-                              row['reason_dm_rx'],
-                              row['non_adherence'],
-                              row['edu_level'],
-                              row['employment_status'],
-                              row['marital_status'],
-                              row['num_twice_daily_pillsy_meds'],
-                              row['pillsy_meds___1'],
-                              row['pillsy_meds___2'],
-                              row['pillsy_meds___3'],
-                              row['pillsy_meds___4'],
-                              row['pillsy_meds___5'],
-                              row['pillsy_meds___6'],
-                              row['pillsy_meds___7'],
-                              row['pillsy_meds___8'],
+                              int(row['sex']),
+                              int(row['num_years_dm_rx']),
+                              int(row['hba1c']),
+                              int(row['race___1']),
+                              int(row['race___2']),
+                              int(row['race___3']),
+                              int(row['race___4']),
+                              int(row['race___5']),
+                              int(row['race___6']),
+                              int(row['race___7']),
+                              int(row['num_physicians']),
+                              int(row['num_rx']),
+                              int(row['concomitant_insulin_use']),
+                              int(row['automaticity']),
+                              int(row['pt_activation']),
+                              int(row['reason_dm_rx']),
+                              int(row['non_adherence']),
+                              int(row['edu_level']),
+                              int(row['employment_status']),
+                              int(row['marital_status']),
+                              int(row['num_twice_daily_pillsy_meds']),
+                              int(row['pillsy_meds___1']),
+                              int(row['pillsy_meds___2']),
+                              int(row['pillsy_meds___3']),
+                              int(row['pillsy_meds___4']),
+                              int(row['pillsy_meds___5']),
+                              int(row['pillsy_meds___6']),
+                              int(row['pillsy_meds___7']),
+                              int(row['pillsy_meds___8']),
                               #row['num_pillsy_meds'],
                               0,
                               0,
@@ -144,6 +144,11 @@ if __name__ == '__main__':
     unique_study_ids_list_redcap = get_redcap_study_ids(redcap_data)
     pt_dict_redcap = update_patient_dict_redcap(unique_study_ids_list_redcap, redcap_data, pt_dict)
     for pt, data in pt_dict_redcap.items():
-        print("\n", pt,",",data.last_run_time, ",",data.start_date,",", str(data.last_run_time), ",",str(data.start_date),",",str( data.age ), ",", str(data.race_black),",", str(data.dichot_adherence_day1), ",",str(data.response_action_id_framing))
+        print("\n This is the record_id", pt)
+        print("This is the start date ", data.start_date)
+        print("checking the counter ", data.trial_day_counter)
+        print("checking the age", data.age)
+        print("checking race_black", data.race_black)
+        print(data.last_run_time, ",", data.start_date,",", str(data.last_run_time), ",",str(data.start_date),",",str( data.age ), ",", str(data.race_black),",", str(data.dichot_adherence_day1), ",",str(data.response_action_id_framing))
         print( pt, ",", data.last_run_time, ",", data.start_date, ", age:", data.age ,",", data.race_black, ",", data.dichot_adherence_day1, ",", data.response_action_id_framing)
     export_pt_dict_pickle(pt_dict_redcap)
