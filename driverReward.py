702586ab23579e32a47f984c9a162cded8a113d9
lgbessette <bessette.l@husky.neu.edu>
2020-12-19 02:22:43 -0500

    need timzone and reward checker

--- driverReward.py
+++ driverReward.py
@@ -60,13 +60,12 @@ def send_rewards(reward_updates, client):
     # column_values = ['reward', '
     #   frame_id', 'history_id', 'social_id', 'content_id', 'reflective_id',
     #   'study_id', 'trial_day_counter']
-    for i in range(0,len(reward_updates)):
+    for i in range(0,reward_updates.shape[0]):
         row = reward_updates[i, :]
-        reward_val = row[0]
+        reward_val = row[0] 
         for j in range(1,6):
             print("reward_val: ", reward_val)
             print("event_id: ", row[j])
-            
             client.events.reward(event_id=row[j], value=reward_val)
             
             # headers = {
